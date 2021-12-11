742ce9fc822edf0148cc036c9989b44736f32a03
Dave Gurnell <dave@cartographer.io>
2021-12-11 11:17:26 +0000

    Add Scaladoc. Fixes #44.

--- src/main/scala-2/unindent/package.scala
+++ src/main/scala-2/unindent/package.scala
@@ -2,7 +2,52 @@ import scala.language.experimental.macros
 
 package object unindent {
   implicit class UnindentSyntax(val ctx: StringContext) extends AnyVal {
+
+    /**
+     * Alternative to `s"..."` that removes the indent applied in the source file.
+     * Example follows:
+     *
+     * {{{
+     * import unindent._
+     *
+     * val example =
+     *   i"""
+     *   This is an indented multi-line string.
+     *   This line ends up unindented.
+     *     This line ends up indented by two spaces.
+     *   It supports interpolation too: ${1 + 1}.
+     *   """
+     *
+     * println("[" + example + "]")
+     * // [This is an indented multi-line string.
+     * // This line ends up unindented.
+     * //   This line ends up indented by two spaces.
+     * // It supports interpolation too: 2.]
+     * }}}
+     */
     def i(args: Any*): String = macro UnindentMacros.unindentMacroUnfolded
+
+    /**
+     * Alternative to `s"..."` that removes the indent applied in the source file
+     * and folds "paragraphs" of text into single lines
+     * (similar to YAML folded multiline strings).
+     * Example follows:
+     *
+     * {{{
+     * val example =
+     *   i1"""
+     *   This is the first line.
+     *   This line is appended to the first.
+     *
+     *   This line follows a line break.
+     *     This line ends up indented by two spaces.
+     *   """
+     *
+     * println("[" + example + "]")
+     * // [This is the first line. This line is appended to the first.
+     * // This line follows a line break.   This line ends up indented by two spaces.]
+     * }}}
+     */
     def i1(args: Any*): String = macro UnindentMacros.unindentMacroFolded
   }
 }
--- src/main/scala-3/unindent/package.scala
+++ src/main/scala-3/unindent/package.scala
@@ -3,7 +3,51 @@ package unindent
 import scala.quoted.*
 
 extension (inline ctx: StringContext)
+  /**
+   * Alternative to `s"..."` that removes the indent applied in the source file.
+   * Example follows:
+   *
+   * {{{
+   * import unindent._
+   *
+   * val example =
+   *   i"""
+   *   This is an indented multi-line string.
+   *   This line ends up unindented.
+   *     This line ends up indented by two spaces.
+   *   It supports interpolation too: ${1 + 1}.
+   *   """
+   *
+   * println("[" + example + "]")
+   * // [This is an indented multi-line string.
+   * // This line ends up unindented.
+   * //   This line ends up indented by two spaces.
+   * // It supports interpolation too: 2.]
+   * }}}
+   */
   inline def i(inline args: Any *): String =
     ${ UnindentMacros.unindentMacro('ctx, 'args, false) }
+
+  /**
+   * Alternative to `s"..."` that removes the indent applied in the source file
+   * and folds "paragraphs" of text into single lines
+   * (similar to YAML folded multiline strings).
+   * Example follows:
+   *
+   * {{{
+   * val example =
+   *   i1"""
+   *   This is the first line.
+   *   This line is appended to the first.
+   *
+   *   This line follows a line break.
+   *     This line ends up indented by two spaces.
+   *   """
+   *
+   * println("[" + example + "]")
+   * // [This is the first line. This line is appended to the first.
+   * // This line follows a line break.   This line ends up indented by two spaces.]
+   * }}}
+   */
   inline def i1(inline args: Any *): String =
     ${ UnindentMacros.unindentMacro('ctx, 'args, true) }
